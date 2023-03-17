97cc0b96a832431ae0a331758860ca3beea7d2fe
Abubacer <azizdesignislife@gmail.com>
2023-03-16 18:39:18 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char Puzzle[] = "\"Pogramming is like building a multilingual puzzle";
+	char Puzzle[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Puzzle);
 
