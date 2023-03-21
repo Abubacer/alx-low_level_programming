0e58ea4bc63df3fd54b083b2d68ae228e8cdf094
Abubacer <azizdesignislife@gmail.com>
2023-03-21 17:06:55 +0000

    the prototype of the function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include "_putchar.c"
+
+/**
+* main - entry point
+* this is a program that prints _putchar
+* followed by a new line.
+* Return: 0 (Success)
+*/
+
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 1 ; i++)
+	{
+		putchar('_');
+		putchar('p');
+		putchar('u');
+		putchar('t');
+		putchar('c');
+		putchar('h');
+		putchar('a');
+		putchar('r');
+	}
+	putchar('\n');
+	return (0);
+}
