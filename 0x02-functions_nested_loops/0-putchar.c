3546e8ebe21456d1ae00958ccee6a80213921495
Abubacer <azizdesignislife@gmail.com>
2023-03-21 17:13:51 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "_putchar.c"
+#include "main.h"
 
 /**
 * main - entry point
@@ -10,19 +10,16 @@
 
 int main(void)
 {
-	int i;
 
-	for (i = 0 ; i < 1 ; i++)
-	{
-		putchar('_');
-		putchar('p');
-		putchar('u');
-		putchar('t');
-		putchar('c');
-		putchar('h');
-		putchar('a');
-		putchar('r');
-	}
-	putchar('\n');
-	return (0);
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+
+		return (0);
 }
