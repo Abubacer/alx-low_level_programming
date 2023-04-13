216b7a4d1b3fae6173cd7497f0a00d4693fcb349
Abubacer <azizdesignislife@gmail.com>
2023-04-13 22:02:52 +0000

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,11 +14,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i, j, slen1, slen2;
 
-	if (s1 == 0 && s2 == 0)
-	{
+	if (s1 == 0)
 		s1 = "";
+	if (s2 == 0)
 		s2 = "";
-	}
+
 	slen1 = 0;
 
 	while (s1[slen1] != '\0')
