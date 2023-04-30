73e3b9c4d994a6a3ab380100559a079501beecae
Abubacer <azizdesignislife@gmail.com>
2023-04-30 17:39:38 +0000

    free and sets head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a linked list
+ * @head: the double pointer to the head
+ *
+ * Return: Void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *nodeint;
+
+	nodeint = *head;
+
+	while (nodeint)
+	{
+		tmp = nodeint;
+		nodeint = nodeint->next;
+		free(tmp);
+	}
+
+	*head = NULL;
+}
