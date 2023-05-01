da509c29ee5088cad27bf0af2901737af24add81
Abubacer <azizdesignislife@gmail.com>
2023-05-01 17:09:13 +0000

    Free abd sets the head to NULL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,6 +11,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (head == 0)
+		return;
+
 	while (*head)
 	{
 		tmp = (*head)->next;
@@ -18,5 +21,5 @@ void free_listint2(listint_t **head)
 		*head = tmp;
 	}
 
-	*head = NULL;
+	*head = 0;
 }
