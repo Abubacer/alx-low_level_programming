3049690e348ed8e7b75881f42328b71574ae5a6b
Abubacer <azizdesignislife@gmail.com>
2023-05-09 12:41:10 +0000

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * main - a function that copies the content of a file to another file
+ * @argc: the argument count
+ * @argv: the argument vector
+ * Return: 1 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int file_from, file_to;
+	char buf_size[BUFSIZE];
+	ssize_t read_byte, write_byte;
+
+	file_from = 0;
+	file_to = 0;
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, ARGS_ERR), exit(97);
+
+	file_from = open(argv[1], O_RDONLY);
+
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, R_ERR, argv[1]), exit(98);
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, W_ERR, argv[2]), exit(99);
+
+	while ((read_byte = read(file_from, buf_size, BUFSIZE)) > 0)
+	{
+		write_byte = write(file_to, buf_size, read_byte);
+		if (write_byte != read_byte)
+		{
+			dprintf(STDERR_FILENO, W_ERR, argv[2]), exit(99);
+		}
+	}
+	if (read_byte == -1)
+		dprintf(STDERR_FILENO, R_ERR, argv[1]), exit(98);
+
+	file_from = close(file_from);
+	file_to = close(file_to);
+
+	if (file_from)
+		dprintf(STDERR_FILENO, CLOSE_ERR, file_from), exit(100);
+	if (file_to)
+		dprintf(STDERR_FILENO, CLOSE_ERR, file_from), exit(100);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,9 +9,17 @@
 #include <sys/stat.h>
 #include <string.h>
 
+#define ARGS_ERR "Usage: cp file_from file_to\n"
+#define R_ERR "Error: Can't read from file %s\n"
+#define W_ERR "Error: Can't write to %s\n"
+#define CLOSE_ERR "Error: Can't close fd %d\n"
+
+#define BUFSIZE 1024
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char **argv);
 
 #endif
